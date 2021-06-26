f746f17887574380dfa4b06ba94cab383d88e44a
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-06-26 14:00:24 -0700

    Completed Array Lab-2

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+cats.lenght;
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+    cats.pop('cats.length - 1')
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift('cats.length')
+}
+function appendCat() {
+    const appendCat = [...cats, 'Broom'];
+    return appendCat;
+}
+function prependCat() {
+    const prependCat = ['Arnold', ...cats];
+    return prependCat;
+}
+function removeLastCat() {
+  cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    cats.slice(1);
+}
+
+function removeLastCat() {
+    const removeLastCat = cats.slice(0, -1);
+    return removeLastCat;
+}
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
+
+
